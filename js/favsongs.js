feda2d21045129740f43e91121ce5435a8c95150
Paul Lamere <Paul.Lamere@gmail.com>
2013-01-28 12:53:16 +0100

    initial commit comment

---
+++ js/favsongs.js
@@ -0,0 +1,56 @@
+var favSongs = [
+    {
+        "artist": "AWOLNATION", 
+        "id": "TRWHQOK13B357AB74A", 
+        "title": "Sail", 
+        "visits": 62
+    }, 
+    {
+        "artist": "Daft Punk", 
+        "id": "TRZKXTL13B5505133D", 
+        "title": "Harder, better, faster, stronger", 
+        "visits": 110
+    }, 
+    {
+        "artist": "Muse", 
+        "id": "TRHIUST13B5695A3E4", 
+        "title": "Madness", 
+        "visits": 62
+    }, 
+    {
+        "artist": "The White Stripes", 
+        "id": "TRMQYSY13B4C10127E", 
+        "title": "Seven Nation Army", 
+        "visits": 29
+    }, 
+    {
+        "artist": "Daft Punk", 
+        "id": "TRSRSLI13451D2B044", 
+        "title": "Around The World", 
+        "visits": 29
+    }, 
+    {
+        "artist": "Skrillex", 
+        "id": "TRPNLNS13991BF1FBC", 
+        "title": "Bangarang (feat. Sirah)", 
+        "visits": 26
+    }, 
+    {
+        "artist": "Skrillex", 
+        "id": "TRECVNM13B542388C0", 
+        "title": "Scary Monsters and Nice Sprites", 
+        "visits": 25
+    }, 
+    {
+        "artist": "The Postal Service", 
+        "id": "TRBXFDO13B52FCCD0B", 
+        "title": "Such Great Heights", 
+        "visits": 23
+    }, 
+    {
+        "artist": "Hans Zimmer", 
+        "id": "TRUTVSC13B550C9BCA", 
+        "title": "Time", 
+        "visits": 22
+    } 
+]
